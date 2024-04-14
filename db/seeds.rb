c6e37f772858e531085df562d960c962ddff1f1f
Zuma-coder <munieru3810@gmail.com>
2024-04-14 13:49:39 +0900

    seedファイルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+8.times do |i|
+    normal_price = [1000, 1500, 2000].sample
+    sales_price = rand < 0.5 ? normal_price / 2 : nil
+
+    product = Product.create!(
+        name: "商品#{i + 1}",
+        code: "ABC#{i + 1}",
+        normal_price: normal_price,
+        sales_price: sales_price,
+        description: "商品#{i + 1}の説明です",
+        created_at: Time.now,
+        updated_at: Time.now
+    )
+
+    image_path = Rails.root.join('storage', "image#{i + 1}.png")
+    product.image.attach(io: File.open(image_path), filename: "image#{i + 1}.png")
+end
