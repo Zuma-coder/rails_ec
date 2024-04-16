a4ed90d2e422f9f6cd2d713089cb03756a2c49fe
Zuma-coder <munieru3810@gmail.com>
2024-04-16 13:09:10 +0900

    N+1問題の解消、product_cardパーシャルを作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,12 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.with_attached_image.all
   end
 
   def show
-    @products = Product.order(created_at: :desc)
+    @products = Product.with_attached_image
+                       .order(created_at: :desc)
                        .limit(4)
     @product = Product.find(params[:id])
   end
